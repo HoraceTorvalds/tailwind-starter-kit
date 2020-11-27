c11ad3ddf75deac88014d20bbf1513c9ea25482b
horaceTorvalds <fredy.masika@gmail.com>
2020-11-27 14:10:11 +0300

    initial commit,

--- Landing Page/angular-landing-page/src/app/app-routing.module.ts
+++ Landing Page/angular-landing-page/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LandingComponent } from "./pages/landing/landing.component";
+import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "landing", pathMatch: "full"},
-  { path: "landing", component: LandingComponent }
+  { path: '', redirectTo: 'landing', pathMatch: 'full'},
+  { path: 'landing', component: LandingComponent }
 ];
 
 @NgModule({
